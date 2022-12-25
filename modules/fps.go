a7ff91a706b77a7619b08f4c169fef66a2c0b004
Kostiantyn Osichenko <kostiantyn.osichenko@recognyte.com>
2022-12-25 23:17:26 +0100

    remove logrus

--- modules/fps.go
+++ modules/fps.go
@@ -2,17 +2,16 @@ package modules
 
 import (
 	"github.com/Gregmus2/simple-engine/common"
-	"github.com/sirupsen/logrus"
 )
 
-func NewFPS(logger *logrus.Entry) common.UpdateActionOut {
+func NewFPS() common.UpdateActionOut {
 	return common.UpdateActionOut{
 		Action: func(dt int64) {
 			if !common.Config.Debug.FPS {
 				return
 			}
 
-			logger.Debug(1 / dt)
+			println(1 / dt)
 		},
 	}
 }
